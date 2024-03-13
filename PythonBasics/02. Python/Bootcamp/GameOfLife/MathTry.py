1e57c52271ff04d97e01557173247091bbbb2584
Manoj Bhargavan <manoj.bhargavan@outlook.in>
2024-03-13 08:33:33 +0530

    prac

---
+++ PythonBasics/02. Python/Bootcamp/GameOfLife/MathTry.py
@@ -0,0 +1,8 @@
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+def add_number(a: float, b: float) -> float:
+    return a + b
+
+def add_number_str(a: str, b: str) -> str:
+    return int(a) + int(b)
